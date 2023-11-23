0d2d5893306c1f78a40cfcc63336d50cbfc01626
swpark <swpark@enliple.com>
2023-11-23 17:47:01 +0900

    초기 셋팅

---
+++ spring-config-service/src/main/java/com/deep/night/demo/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.deep.night.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApplication.class).run(args);
+    }
+}
