f18fc8e608bd0be1d360b5d2e87e96c5e48df667
swpark <swpark@enliple.com>
2024-01-12 11:51:23 +0900

    swagger 적용

--- demo-service/src/main/java/com/deep/night/demo/config/SwaggerConfig.java
+++ demo-service/src/main/java/com/deep/night/demo/config/SwaggerConfig.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(title = "demo-Service API 명세서",
@@ -26,6 +28,7 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
+                .servers(List.of(new Server().url("http://localhost:7733")))
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
                 .security(Arrays.asList(securityRequirement));
     }
