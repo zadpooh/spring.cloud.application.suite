fd1d741003b1f8393d2ae0acc6275848ce6ec68d
박승우 <swpark@dnasoft.co.kr>
2024-03-28 16:19:09 +0900

    init

--- user-auth-service/src/main/java/com/deep/night/user/AuthController.java
+++ user-auth-service/src/main/java/com/deep/night/user/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @EnableAutoConfiguration
 @RestController
-@RequestMapping("/auth/user")
+//@RequestMapping("/auth/user")
 @Slf4j
 public class AuthController {
 
